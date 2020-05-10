aae8ed863917e434336741f3292c85e8011f117a
pphillips1002 <55248632+pphillips1002@users.noreply.github.com>
2020-05-10 05:09:40 -0400

    Started Data Modeling

---
+++ data_modeling.py
@@ -0,0 +1,11 @@
+##  05/10/2020
+##  Author: Pierce Phillips
+##  Data Modeling
+##  Salary Prediction Project
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('eda_data.csv')
+
